8eddbd518974de00772418912557d306104dd955
lindseykim6 <lindseykim06@gmail.com>
2023-04-12 04:59:32 -0400

    fixed eslint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import eslint from 'vite-plugin-eslint';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
